c8c477863592cb9f76292a3878988a13e7f43ca0
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-25 13:10:14 -0700

    build bootstrap responsive navbar

--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar bg-light">
-        <div className="container">
-          <Link className="navbar-brand" href="#">
-            <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" />
-          </Link>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
         </div>
     </nav>
   )
